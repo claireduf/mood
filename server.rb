41a532b8092e3597afea8c7d76aa5b070e5592c6
Claire Dufetrelle <claire.dufetrelle@protonmail.com>
2018-08-18 14:32:31 +0200

    Add routes required by Clarke

--- server.rb
+++ server.rb
@@ -1,7 +1,16 @@
 require "sinatra"
 require_relative "./database"
+require 'clarke'
+require 'clarke/platformlib'
+require 'clarke/messenger'
 
-get "/current_mood.json" do
-  current_mood = Mood::Database.database[:moods].order(:id).last
-  current_mood.to_json
+Clarke::Messenger::Config.facebook_page_token = 'your_facebook_page_token'
+
+post '/messenger' do
+  request_body = JSON.parse(request.body.read)
+  Clarke.process_request(Clarke::Messenger, request_body)
+
+post '/platform' do
+  request_body = JSON.parse(request.body.read)
+  Clarke.process(Clarke::PlatformLibrary, request_body)
 end
